3c2da4592fce0d7ba9a6d6cf7cf9c8eb9f959cd4
Devkorrir <kipyegonaldo@proton.me>
2026-06-05 18:49:30 +0300

    refactor: remove unused imports from ShambaGuardNavGraph

--- android/ShambaGuard/app/src/main/java/dev/korryr/shambaguard/navigation/ShambaGuardNavGraph.kt
+++ android/ShambaGuard/app/src/main/java/dev/korryr/shambaguard/navigation/ShambaGuardNavGraph.kt
@@ -3,7 +3,6 @@ package dev.korryr.shambaguard.navigation
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +15,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
