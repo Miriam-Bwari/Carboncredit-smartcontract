f9c7bc7998fe89c8dbae3d56d32b6bf987c41fe8
Miriam-Bwari <miriamomwoyo4@gmail.com>
2026-06-12 16:00:08 +0300

    Fix: Adjusted deployment script configuration paths

--- script/DeployCarbonCredit.s.sol
+++ script/DeployCarbonCredit.s.sol
@@ -8,7 +8,7 @@ import "../src/CarbonCredit.sol";
 contract DeployCarbonCredit is Script {
     function run() external {
         // Retrieve the private key from your local system environment variables
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint(e09391018b621a3125b1b0b6d93c108aa3fb66b835a08aebc0b7ccd3b2940aaa);
 
         // Everything inside the broadcast block will create a real broadcasted transaction
         vm.startBroadcast(deployerPrivateKey);
